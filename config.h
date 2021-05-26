273bb1040843afa1fba16b4c76d715012a182b30
zawy <46110056+zoeyalex@users.noreply.github.com>
2021-05-26 22:01:48 +0200

    change font parameters

--- config.h
+++ config.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "Tamzen:pixelsize=16:antialias=true:autohint=true";
+static char *font = "Tamzen:pixelsize=15:antialias=true:autohint=true:style=Regular";
 static int borderpx = 2;
 
 /*
